0c2892689d14c7971ec0249489fb7035855e5c67
Yoshio HANAWA <y@hnw.jp>
2017-01-18 18:34:18 +0900

    Docker Hubのautomated buildでベースイメージを生成し、Travis CIでtoolchainをビルドしてDocker Hubにデプロイするようにした / Travis CIのccacheの仕組みをDocker側で使えるようにした / .configの設定で並列ビルドを有効にした / READMEに日本語説明を書いた

--- README.md
+++ README.md
@@ -13,8 +13,35 @@ the user `openwrt` was created. The buildroot can be found in
 To run a shell in the buildroot, execute the following command:
 
 ```sh
-docker run -it -u openwrt yhnw/openwrt-buildroot:15.05.1 bash
+docker run -it -u openwrt yhnw/openwrt-buildroot:15.05.1-ar71xx bash
 ```
-
 More information on how to use this buildroot can be found on the
 [OpenWRT wiki](http://wiki.openwrt.org/doc/howto/build).
+
+
+
+docker-openwrt-buildroot
+========================
+
+[OpenWRT](https://openwrt.org/)の[buildroot](http://wiki.openwrt.org/doc/howto/buildroot.exigence)環境をDockerイメージで提供するプロジェクトです。
+
+Travis CIを利用し、OpenWrt buildrootのtoolchainまでビルドした状態でDocker Hubにデプロイしています。一番時間のかかるtoolchainの生成が既に済んでいる状態のDockerイメージがあると自前ビルドの心理的ハードルが下がるのではないでしょうか（個人的には、自前の`*.ipk`が作りたいだけならOpenWrt SDKの利用をオススメします）。
+
+このイメージは下記のようにすれば使えます。
+
+```
+$ docker run -it -u openwrt yhnw/openwrt-buildroot:15.05.1-ar71xx bash
+# cd openwrt
+# scripts/feeds update -a
+```
+
+ここで`make menuconfig`や`make kernel_menuconfig`などで設定をしてから`make`すれば`bin/`以下にカスタムファームウェアが作られます。
+
+注意点ですが、Dockerイメージのダイエットのため`dl/`以下のアーカイブファイルを0バイトに切り詰めてあります。この状態でも`make`を行うだけなら問題がないのですが、`make toolchain/install`などとツール類を再作成しようとするとエラーで怒られますので、その際は一度`dl/`以下の全ファイルを削除してください。
+
+現時点では下記の組み合わせのイメージしか提供していません。必要な組み合わせがあればissueなりpull requestなりからご連絡ください。
+
+ * OpenWrtバージョン
+   - 15.05.1
+ * 対応ボード
+   - ar71xx（Atheros AR71xx/AR7240/AR913x/AR934x）
--- build.sh
+++ build.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
+set -e
 
-OPENWRT_VER=$1
-ARCH=$2
-PATCH_FILE="/work/patch/config.${ARCH}.patch"
+BRANCH=$1
+BOARD=$2
+PATCH_FILE="/work/patch/config.${BRANCH}-${BOARD}.patch"
 
-[[ ! -e "${PATCH_FILE}" ]] && echo "ERROR: cannot access ${PATCH_FILE}" && exit 1
+[[ ! -e ${PATCH_FILE} ]] && echo "ERROR: cannot access ${PATCH_FILE}" && exit 1
 
-cd ${HOME}
-if [[ "${OPENWRT_VER}" == "trunk" ]] ; then
-    git clone git://github.com/openwrt/openwrt.git
-elif [[ "${OPENWRT_VER}" == "15.05.1" ]] ; then
-    git clone -b chaos_calmer git://github.com/openwrt/openwrt.git
-elif [[ "${OPENWRT_VER}" == "14.07" ]] ; then
-    git clone -b barrier_breaker git://github.com/openwrt/openwrt.git
-else
-    echo "ERROR: unknown version specified: ${OPENWRT_VER}" && exit 1   
+# Use TravisCI's ccache directory in build
+if [[ -d ${HOME}/.ccache ]] ; then
+    for dir in host ${TARGET} ${TOOLCHAIN}; do
+        mkdir -p ${HOME}/.ccache/${dir}
+        if [[ ! -d ${HOME}/openwrt/staging_dir/${dir} ]] ; then
+            mkdir -p ${HOME}/openwrt/staging_dir/${dir}
+        fi
+        if [[ -e ${HOME}/openwrt/staging_dir/${dir}/ccache ]] ; then
+            mv ${HOME}/openwrt/staging_dir/${dir}/ccache ${HOME}/openwrt/staging_dir/${dir}/ccache.orig
+        fi
+        ln -s ${HOME}/.ccache/${dir} ${HOME}/openwrt/staging_dir/${dir}/ccache
+        CCACHE_DIR=${HOME}/.ccache/${dir} ccache -s | tee /tmp/ccache.${dir}.log
+    done
 fi
-cd openwrt
+
+# Build toolchain
+cd ${HOME}/openwrt
 scripts/feeds update -a
 patch -p1 < "${PATCH_FILE}"
-grep -v '^#' .config | uniq
-CCACHE_DIR=staging_dir/host/ccache/ ccache -s
-make prepare
-CCACHE_DIR=staging_dir/host/ccache/ ccache -s
+make toolchain/install
+
+# Cleanup
+find build_dir \( -type l -or -type f \) -not -name '.*' -print0 | xargs -0 rm
+find build_dir -type d -empty -delete
+rm -f dl/{linux,u-boot}-*
+for file in dl/* ; do
+    mv ${file} ${file}.orig
+    touch ${file} -r ${file}.orig
+    rm ${file}.orig
+done
+
+# Show ccache status and remove them from image
+for dir in host ${TARGET} ${TOOLCHAIN}; do
+    if [[ -L ${HOME}/openwrt/staging_dir/${dir}/ccache ]] ; then
+        echo "### ccache status (before build, ${dir}) ###"
+        cat /tmp/ccache.${dir}.log
+        echo ""
+        echo "### ccache status (after build, ${dir}) ###"
+        CCACHE_DIR=${HOME}/.ccache/${dir} ccache -s
+        rm -f /tmp/ccache.${dir}.log
+        rm -f ${HOME}/openwrt/staging_dir/${dir}/ccache
+    fi
+done
